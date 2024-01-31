d86c6d59fcf7eba1468ebfd73bbf076da8caf76f
Kevin Fahrenberger <fahrenberger.k@web.de>
2024-01-31 16:11:43 +0100

    add: two ul

--- 01/index.js
+++ 01/index.js
@@ -1,4 +1,7 @@
 ReactDOM.render(
-    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
-    document.getElementById("root")
-)
+  <li>
+    <ul>Item1</ul>
+    <ul>Item2</ul>
+  </li>,
+  document.getElementById("root")
+);
